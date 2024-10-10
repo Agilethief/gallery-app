c5bdc33fba21b5c0880b18a6eb76dbd428c476b2
Agilethief <tbermanseder@gmail.com>
2024-10-11 00:00:20 +1100

    Modal setup with bootstrap now working and showing image correctly.

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,7 +1,9 @@
-import { Component, inject, ElementRef, ViewChild } from '@angular/core';
+import { Component, inject, ElementRef, ViewChild, Input } from '@angular/core';
 import { PhotosService } from '../photos.service';
 import { PhotoTileComponent } from '../photo-tile/photo-tile.component';
 import { PhotoData } from '../photo.params.model';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
 // This component will provide a list of images that belong to the specified gallery
 
 @Component({
@@ -14,6 +16,7 @@ import { PhotoData } from '../photo.params.model';
 export class GalleryComponent {
   photos: PhotoData[] = [];
   photosService = inject(PhotosService);
+  private modalService = inject(NgbModal);
 
   @ViewChild('imageModal') imageModal!: ElementRef<HTMLDivElement>;
 
@@ -23,6 +26,41 @@ export class GalleryComponent {
 
   showImageModal(photo: PhotoData) {
     // Show the image modal
-    this.imageModal.nativeElement.style.display = 'block';
+    const modalRef = this.modalService.open(NgbdModalContent, {
+      size: 'lg', // Use 'lg' or 'xl' for predefined sizes
+      //windowClass: 'custom-modal', // Apply custom class if needed
+    });
+    modalRef.componentInstance.photoData = photo;
   }
+}
+
+@Component({
+  selector: 'ngbd-modal-content',
+  standalone: true,
+  template: `
+    <div class="modal-header">
+      <h4 class="modal-title">{{ photoData.title }}</h4>
+      <button
+        type="button"
+        class="btn-close"
+        aria-label="Close"
+        (click)="activeModal.dismiss('Cross click')"
+      ></button>
+    </div>
+    <div class="modal-body px-0">
+      <div class="ratio ratio-1x1 rounded-sm">
+        <img
+          src="{{ photoData.src }}"
+          alt="{{ photoData.alt }}"
+          class="img-fluid rounded-sm"
+          style="object-fit: cover;"
+        />
+      </div>
+    </div>
+  `,
+})
+export class NgbdModalContent {
+  activeModal = inject(NgbActiveModal);
+
+  @Input() photoData!: PhotoData;
 }
--- src/app/photo-tile/photo-tile.component.ts
+++ src/app/photo-tile/photo-tile.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, EventEmitter, Output } from '@angular/core';
 import { PhotoData } from '../photo.params.model';
 // Represents a single photo in the gallery
 // Will be the smaller thumbnail version
@@ -13,8 +13,10 @@ import { PhotoData } from '../photo.params.model';
 })
 export class PhotoTileComponent {
   @Input() photoData!: PhotoData;
+  @Output() photoClick = new EventEmitter<PhotoData>();
 
   onButtonClick(buttonId: number) {
-    console.log(`Photo ${buttonId} clicked!`);
+    console.log(`Photo ${buttonId} clicked! for ${this.photoData.title}`);
+    this.photoClick.emit(this.photoData);
   }
 }
