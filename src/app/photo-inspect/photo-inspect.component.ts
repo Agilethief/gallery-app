3facc38ac742f0d2303a7d808f3dde171dfa7477
Agilethief <tbermanseder@gmail.com>
2024-10-13 16:42:53 +1100

    Initial s3 auto deploy setup

--- src/app/photo-inspect/photo-inspect.component.ts
+++ src/app/photo-inspect/photo-inspect.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PhotoData } from '../photo.params.model';
 import { PhotosService } from '../photos.service';
+import { ShareBarComponent } from '../share-bar/share-bar.component';
+
 // The zoomed in view of a photo, with share icons, QR code generation etc.
 // Should display the full image
 // Is its own PAGE, not a modal
@@ -9,7 +11,7 @@ import { PhotosService } from '../photos.service';
 @Component({
   selector: 'app-photo-inspect',
   standalone: true,
-  imports: [],
+  imports: [ShareBarComponent],
   templateUrl: './photo-inspect.component.html',
   styleUrl: './photo-inspect.component.css',
 })
--- src/app/share-bar/share-bar.component.ts
+++ src/app/share-bar/share-bar.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { PhotoData } from '../photo.params.model';
 
 @Component({
   selector: 'app-share-bar',
   standalone: true,
   imports: [],
   templateUrl: './share-bar.component.html',
-  styleUrl: './share-bar.component.css'
+  styleUrl: './share-bar.component.css',
 })
 export class ShareBarComponent {
+  @Input() photoData!: PhotoData;
 
+  onShareClick(shareType: string) {
+    console.log(`Sharing ${this.photoData.title} on ${shareType}`);
+  }
+  onReportClick() {
+    console.log(`Reporting ${this.photoData.title}`);
+  }
+  onQRClick() {
+    console.log(`Generating QR code for ${this.photoData.title}`);
+  }
 }
